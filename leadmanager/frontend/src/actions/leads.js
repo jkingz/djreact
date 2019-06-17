7dd90ba036afda5d2aefdbe4d2cc45f2d2e3a45a
jkingz <jfkrule@gmail.com>
2019-06-18 02:52:36 +0800

    Create actions to get the leads from the backend

---
+++ leadmanager/frontend/src/actions/leads.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+import { GET_LEADS } from "./types";
+
+//GET LEADS
+export const getLeads = () => dispatch => {
+  axios
+    .get("/apiv1/leads/")
+    .then(res => {
+      console.log(res.data);
+      dispatch({
+        type: GET_LEADS,
+        payload: res.data
+      });
+    })
+    .catch(err => console.log(err));
+};
