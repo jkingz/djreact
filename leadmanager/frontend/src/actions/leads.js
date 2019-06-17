289a0a73049ad702ae8f13fcb4800f713dd376e8
jkingz <jfkrule@gmail.com>
2019-06-18 03:19:52 +0800

    Create actions to delete the lead

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_LEADS } from "./types";
+import { GET_LEADS, DELETE_LEAD } from "./types";
 
 //GET LEADS
 export const getLeads = () => dispatch => {
@@ -10,6 +10,19 @@ export const getLeads = () => dispatch => {
       dispatch({
         type: GET_LEADS,
         payload: res.data
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+// DELETE LEADS
+export const deleteLead = id => dispatch => {
+  axios
+    .delete(`/apiv1/leads/${id}/`)
+    .then(res => {
+      dispatch({
+        type: DELETE_LEAD,
+        payload: id
       });
     })
     .catch(err => console.log(err));
