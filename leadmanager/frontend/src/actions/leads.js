df6a9acac8c7c407c78e40107a509d945bc2c18a
jkingz <jfkrule@gmail.com>
2019-06-19 00:52:47 +0800

    Added actions for add and delete lead alerts

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,5 +1,13 @@
 import axios from "axios";
-import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from "./types";
+import { CREATE_MESSAGE, createMessage } from "./messages";
+
+import {
+  GET_LEADS,
+  DELETE_LEAD,
+  ADD_LEAD,
+  GET_ERRORS,
+  GET_MESSAGES
+} from "./types";
 
 //GET LEADS
 export const getLeads = () => dispatch => {
@@ -19,6 +27,11 @@ export const deleteLead = id => dispatch => {
   axios
     .delete(`/apiv1/leads/${id}/`)
     .then(res => {
+      dispatch(
+        createMessage({
+          deleteLead: "LEAD DELETED!"
+        })
+      );
       dispatch({
         type: DELETE_LEAD,
         payload: id
@@ -32,11 +45,24 @@ export const addLead = lead => dispatch => {
   axios
     .post("/apiv1/leads/", lead)
     .then(res => {
-      console.log(res.data);
+      dispatch(
+        createMessage({
+          addLead: "LEAD ADDED!"
+        })
+      );
       dispatch({
         type: ADD_LEAD,
         payload: res.data
       });
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      const errors = {
+        msg: err.response.data,
+        status: err.response.status
+      };
+      dispatch({
+        type: GET_ERRORS,
+        payload: errors
+      });
+    });
 };
