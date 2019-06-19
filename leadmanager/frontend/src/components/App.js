3c77a492a4d9b963b4ee238a7ecb79ab949ce108
jkingz <jfkrule@gmail.com>
2019-06-19 23:31:06 +0800

    Created private rout component

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,5 +1,11 @@
 import React, { Component, Fragment } from "react";
 import ReactDOM from "react-dom";
+import {
+  HashRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
 
 import { Provider as AlertProvider, positions } from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
@@ -7,6 +13,9 @@ import AlertTemplate from "react-alert-template-basic";
 import Header from "./layout/Header";
 import Dashboard from "./leads/Dashboard";
 import Alerts from "./layout/Alerts";
+import Login from "./accounts/Login";
+import Register from "./accounts/Register";
+import PrivateRoute from "./common/PrivateRoute";
 
 import { Provider } from "react-redux";
 import store from "../store";
@@ -22,13 +31,19 @@ class App extends Component {
     return (
       <Provider store={store}>
         <AlertProvider template={AlertTemplate} {...alertOptions}>
-          <Fragment>
-            <Header />
-            <Alerts />
-            <div className="container">
-              <Dashboard />
-            </div>
-          </Fragment>
+          <Router>
+            <Fragment>
+              <Header />
+              <Alerts />
+              <div className="container">
+                <Switch>
+                  <PrivateRoute exact path="/" component={Dashboard} />
+                  <Route exact path="/register" component={Register} />
+                  <Route exact path="/login" component={Login} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
         </AlertProvider>
       </Provider>
     );
